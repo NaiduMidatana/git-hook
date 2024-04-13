01b23507ae491e4ddb5b566da209ba8758c09808
venkayya naidu midatana <venkayyam.in@mouritech.com>
2024-04-13 11:58:40 +0530

    secret keys

---
+++ test.js
@@ -0,0 +1,9 @@
+
+                
+                                   
+console.log("hello world")
+                                   
+console.log("hello world")
+
+let PASSWORD="jdkd";
+let API_KEY = "jhbjgdgjbjbj"
